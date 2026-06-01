1f01ee58630a42fcc5c0b43d50018aec0db56cee
ljk1331ljk <limitless1331@gmail.com>
2026-06-01 14:22:26 +0800

    Prepare beta 5 firmware release

--- ports/esp32/boards/EVOX1P/boot.py
+++ ports/esp32/boards/EVOX1P/boot.py
@@ -1,12 +1,3 @@
 from evo import *
 
 evo_init()
-
-try:
-    import os
-    os.stat("/main.py")
-except OSError:
-    try:
-        import EvoDefaultMain
-    except Exception as e:
-        print("Default program failed:", e)
--- ports/esp32/boards/EVOX1P/manifest.py
+++ ports/esp32/boards/EVOX1P/manifest.py
@@ -4,5 +4,5 @@ include("$(PORT_DIR)/boards/manifest.py")
 # Freeze custom Evo modules
 freeze("$(PORT_DIR)/frozen/evo")
 freeze("$(BOARD_DIR)", "boot.py")
-freeze("$(BOARD_DIR)", "EvoDefaultMain.py")
+freeze("$(BOARD_DIR)", "main.py")
 module("pins.py", base_path=".")
