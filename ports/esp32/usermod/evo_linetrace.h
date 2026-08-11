bdf9ef37b85a4ffcc0683c239f9488780d943ecb
ljk1331ljk <limitless1331@gmail.com>
2026-08-11 12:47:06 +0800

    Add native EvoLineTrace module

---
+++ ports/esp32/usermod/evo_linetrace.h
@@ -0,0 +1,12 @@
+#ifndef EVO_LINETRACE_H
+#define EVO_LINETRACE_H
+
+#include "py/obj.h"
+
+extern const mp_obj_type_t evo_linetrace_type;
+
+#define EVO_JUNCTION_LEFT  (0)
+#define EVO_JUNCTION_RIGHT (1)
+#define EVO_JUNCTION_BOTH  (2)
+
+#endif // EVO_LINETRACE_H
